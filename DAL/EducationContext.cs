786968120279d3ea62fdbb4563a919a957fae402
Дарья Чернова <dariashvartz@mail.ru>
2018-12-09 21:04:34 +0300

    add Education Context for database connection

---
+++ DAL/EducationContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Education.Models;
+
+namespace Education.DAL
+{
+    public class EducationContext : DbContext
+    {
+        public EducationContext(DbContextOptions<EducationContext> options) : base(options) {}
+        public DbSet<Course> Courses { get; set; }
+    }
+}
