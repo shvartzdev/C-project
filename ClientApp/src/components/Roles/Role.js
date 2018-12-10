a350eff0d9e9b34e0dbf10e2fb191309fc11ad40
Дарья Чернова <dariashvartz@mail.ru>
2018-12-11 00:03:02 +0300

    add a react role component

---
+++ ClientApp/src/components/Roles/Role.js
@@ -0,0 +1,22 @@
+{/* <h1>Roles control</h1>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              
+            />
+            <InputGroup.Button>
+              <Button onClick={this.handleAdd}>Add new role</Button>
+            </InputGroup.Button>
+          </InputGroup>
+        </FormGroup>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table> */}
