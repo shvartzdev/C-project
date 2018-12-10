773bb56213bfbdaf33d99f63616a05c01c53a035
Дарья Чернова <dariashvartz@mail.ru>
2018-12-10 11:16:49 +0300

    change migrations files because of database update

---
+++ Migrations/20181209194525_CourseDuration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Education.Migrations
+{
+    public partial class CourseDuration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Duration",
+                table: "Courses",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "Courses",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Courses");
+        }
+    }
+}
---
+++ Migrations/20181210081301_UserAndRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Education.Migrations
+{
+    public partial class UserAndRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Courses",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Courses",
+                newName: "description");
+        }
+    }
+}
