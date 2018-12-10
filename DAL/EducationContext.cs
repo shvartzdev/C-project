a837cc4f447a384fd9ffa2c86fcfdc6207aa954f
Дарья Чернова <dariashvartz@mail.ru>
2018-12-10 11:18:48 +0300

    change EducationContext because of database update

--- DAL/EducationContext.cs
+++ DAL/EducationContext.cs
@@ -5,7 +5,7 @@ namespace Education.DAL
 {
     public class EducationContext : DbContext
     {
-        public EducationContext(DbContextOptions<EducationContext> options) : base(options) {}
+        public EducationContext(DbContextOptions<EducationContext> options) : base(options) { }
         public DbSet<Course> Courses { get; set; }
     }
 }
