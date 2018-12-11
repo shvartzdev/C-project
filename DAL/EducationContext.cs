0ac20f54d2a6066efc72e235ba7a92b33558b75a
Дарья Чернова <dariashvartz@mail.ru>
2018-12-11 17:25:49 +0300

    add new dbsets

--- DAL/EducationContext.cs
+++ DAL/EducationContext.cs
@@ -8,5 +8,9 @@ namespace Education.DAL
         public EducationContext(DbContextOptions<EducationContext> options) : base(options) { }
         public DbSet<Course> Courses { get; set; }
         public DbSet<User> Users {get; set;}
+        public DbSet<Role> Roles {get; set;}
+        public DbSet<Group> Groups {get;set;}
+        public DbSet<Theme> Themes {get;set;}
+        public DbSet<Task> Tasks {get;set;}
     }
 }
