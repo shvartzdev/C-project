3384003e9bfd1e4eba160b3c3e2e72172fba2cb3
Дарья Чернова <dariashvartz@mail.ru>
2018-12-11 17:08:42 +0300

    add task model

---
+++ Models/Tasks/Task.cs
@@ -0,0 +1,9 @@
+namespace Education.Models
+{
+    public class Task
+    {
+        public int TaskId {get;set;}
+        public string TaskAnswer {get;set;}
+        public string TaskQuestion {get;set;}
+    }
+}
