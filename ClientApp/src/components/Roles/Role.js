add778b183f8859f2a9a5e339436bd1fdfaee66a
Дарья Чернова <dariashvartz@mail.ru>
2018-12-12 17:52:33 +0300

    change role component

--- ClientApp/src/components/Roles/Role.js
+++ ClientApp/src/components/Roles/Role.js
@@ -7,6 +7,9 @@ import {
   FormControl
 } from "react-bootstrap";
 import AddRoleModal from './AddRoleModal';
+import UpdateRoleModal from './UpdateRoleModal';
+
+import '../Courses/styles.css';
 
 export default class Roles extends Component {
   constructor(props, context) {
@@ -25,6 +28,16 @@ export default class Roles extends Component {
     };
   }
 
+  handleDelete = (roleId) => {
+    console.log("id", roleId);
+    if (!window.confirm("Are you sure you want to delete the user?")) return;
+    fetch("api/role/delete/" + roleId, { method: "delete" })
+      .then(responce => responce.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ roles: data.roles })
+      })
+  }
  
 
 
@@ -53,31 +66,20 @@ export default class Roles extends Component {
     this.setState({showUpdationModal: !showUpdationModal})
 }
 
-renderRoleTable = (role) => {
+renderRoleBlock = (role) => {
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr key={role.roleID}>
-          <td>{role.name}</td>
-          <td>
-
-            <Button>Edit</Button>
-            <Button>Delete</Button>
-          </td>
-        </tr>
-
-      </tbody>
-    </table>
+    <div>
+      <div className="course" key={role.roleID}>
+        <h1>{role.name}</h1>
+        <p><UpdateRoleModal role={this.state.role} roleID = {role.roleID}/></p>
+        <p><Button onClick={() => this.handleDelete(role.roleID)}>Delete</Button></p>
+      </div>
+    </div>
   );
 }
 
 render = () => {
-  let cards = this.state.roles ? (this.state.roles.map(role => this.renderRoleTable(role))) : <div>Loading</div>;
+  let cards = this.state.roles ? (this.state.roles.map(role => this.renderRoleBlock(role))) : <div>Loading</div>;
   return (
     <div>
       <h1>Roles</h1>
