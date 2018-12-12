db6aae413ed6256159dcf01a63e33abdec22a982
Дарья Чернова <dariashvartz@mail.ru>
2018-12-12 17:48:24 +0300

    add new migrations

---
+++ Migrations/20181212081608_AddThemesInTheCourse.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Education.Migrations
+{
+    public partial class AddThemesInTheCourse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CourseId",
+                table: "Themes",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Themes_CourseId",
+                table: "Themes",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Themes_Courses_CourseId",
+                table: "Themes",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Themes_Courses_CourseId",
+                table: "Themes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Themes_CourseId",
+                table: "Themes");
+
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "Themes");
+        }
+    }
+}
---
+++ Migrations/20181212092810_AddTasksCollection.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Education.Migrations
+{
+    public partial class AddTasksCollection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ThemeID",
+                table: "Tasks",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_ThemeID",
+                table: "Tasks",
+                column: "ThemeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Themes_ThemeID",
+                table: "Tasks",
+                column: "ThemeID",
+                principalTable: "Themes",
+                principalColumn: "ThemeID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Themes_ThemeID",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_ThemeID",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "ThemeID",
+                table: "Tasks");
+        }
+    }
+}
