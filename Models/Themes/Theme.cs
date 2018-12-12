8e69c127c1bbebf96c4c34176f5b92bc4a901876
Дарья Чернова <dariashvartz@mail.ru>
2018-12-12 17:53:44 +0300

    modify theme implementation

--- Models/Themes/Theme.cs
+++ Models/Themes/Theme.cs
@@ -1,8 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Education.Models
 {
     public class Theme
     {
         public int ThemeID {get; set;}
         public string ThemeName {get;set;}
+         public ICollection<Task> Tasks { get; set; }
+        
     }
 }
