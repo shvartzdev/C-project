1d6e3ef40045a362901d7f49dcd3731dba0b6d05
Дарья Чернова <dariashvartz@mail.ru>
2018-12-18 00:06:17 +0300

    configure startup to use jwt

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using Education.DAL;
+using Education.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,9 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 
 namespace Education
 {
@@ -21,8 +25,17 @@ namespace Education
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFrameworkSqlServer()
+                    .AddDbContext<UserDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("UsersContext")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<UserDbContext>();
+
+            services.Configure<JWTSettings>(Configuration.GetSection("JWTSettings"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<EducationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("EducationContext")));
+
+            
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -34,6 +47,8 @@ namespace Education
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseIdentity();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
