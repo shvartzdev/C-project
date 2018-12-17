953a53c3060c438398ba0be4b141cd74e5673fe8
Дарья Чернова <dariashvartz@mail.ru>
2018-12-18 00:05:23 +0300

    add new configutaion
    because of new user database new migrations was needed. there're new connection string in appsettings.json, new packages in package-lock.json (because jwt, identity and others was needed. also add new files with migration description.

---
+++ Migrations/UserDb/20181217110052_AddSignInModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Education.Migrations.UserDb
+{
+    public partial class AddSignInModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "signinmodels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_signinmodels", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "signinmodels");
+        }
+    }
+}
